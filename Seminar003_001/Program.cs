8b1e32853bc2dc7cb79f324c038a0394c20318a0
DmitryKitsenko <Tpalala1993@mail.ru>
2023-09-30 23:39:11 +0300

    added Task8

--- Seminar003_001/Program.cs
+++ Seminar003_001/Program.cs
@@ -29,14 +29,36 @@
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-Console.WriteLine("Input a number");
-int number = Convert.ToInt32(Console.ReadLine());
-if ((number % 2) == 0)
-    {
-        Console.WriteLine($"Number {number} is even :)");
-    }
-    else
-    {
-        Console.WriteLine($" Number {number} is odd :(");
-    }
+// Console.WriteLine("Input a number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if ((number % 2) == 0)
+//     {
+//         Console.WriteLine($"Number {number} is even :)");
+//     }
+//     else
+//     {
+//         Console.WriteLine($" Number {number} is odd :(");
+//     }
 
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Input number");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = N;
+if (N==0)
+   Console.WriteLine($" Have no even numbers at 0 to 0 :("); 
+if (N>0)
+     if ((N % 2) == 0)
+    {for(int i=0; i<=N-2; i=i+2)
+            Console.Write($"{i} ");
+        }
+        else for(int i=0; i<=N-1; i=i+2)
+            Console.Write($"{i} ");
+else if (N<0)
+        if ((N % 2) == 0)
+            {for(int i=0; i>=N+2; i=i-2)
+            Console.Write($"{i} ");
+        }
+        else for(int i=0; i>=N+1; i=i-2)
+            Console.Write($"{i} ");
+    
