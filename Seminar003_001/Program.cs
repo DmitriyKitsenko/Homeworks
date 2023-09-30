709d6bf4e4121319a768d2c0be08af2d34ff6a27
DmitryKitsenko <Tpalala1993@mail.ru>
2023-09-30 21:49:53 +0300

    task2

--- Seminar003_001/Program.cs
+++ Seminar003_001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Input first number");
+int FirstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int SecNum = Convert.ToInt32(Console.ReadLine());
+if (FirstNum == SecNum)
+    Console.WriteLine(":( Not different numbers input, reinput, pls ");
+
+    else if (FirstNum>SecNum){
+    Console.WriteLine($"Number {FirstNum} more than number {SecNum}");
+    }
+    else {
+        Console.WriteLine($"Number {SecNum} more than number {FirstNum}");
+    }
